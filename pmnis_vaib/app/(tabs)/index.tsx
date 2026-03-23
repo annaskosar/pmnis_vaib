d7b15a851a0dc231d14959d73f12961418133eeb
annaskosar <annaskosar2005@gmail.com>
2026-03-23 21:07:30 +0100

    zaciatok

---
+++ pmnis_vaib/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+      <Text>Ahoj svet!</Text>
+    </View>
+  );
+}
